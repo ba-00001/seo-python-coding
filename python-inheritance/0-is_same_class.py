10d537c5931cbdcbd84e094ac6ab529e9696d511
ba-00001 <ba.00001@outlook.com>
2023-06-14 19:54:19 -0400

    update

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -3,11 +3,11 @@ def is_same_class(obj, a_class):
     return type(obj) is a_class
 
 
-a = 1
-if is_same_class(a, int):
-    "{} is an instance of the class {}".format(a, int.__name__)
-if is_same_class(a, float):
-    "{} is an instance of the class {}".format(a, float.__name__)
-if is_same_class(a, object):
-    "{} is an instance of the class {}".format(a, object.__name__)
+# a = 1
+# if is_same_class(a, int):
+#     "{} is an instance of the class {}".format(a, int.__name__)
+# if is_same_class(a, float):
+#     "{} is an instance of the class {}".format(a, float.__name__)
+# if is_same_class(a, object):
+#     "{} is an instance of the class {}".format(a, object.__name__)
 
