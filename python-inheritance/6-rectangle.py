9c456f67ecc23773785ace483520cfee4f6678b1
ba-00001 <ba.00001@outlook.com>
2023-06-19 01:52:43 -0400

    update

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python3
-"""
-Module: rectangle
-
-This module provides a class called Rectangle,
-which inherits from the BaseGeometry class. The Rectangle
-class represents a rectangle shape and provides functionality for calculating its area.
-
-Classes:
-    - Rectangle: A class representing a rectangle shape.
-
-Exceptions:
-    - TypeError: Raised when the value is not an integer.
-    - ValueError: Raised when the value is less than or equal to 0.
-"""
+"""Module: rectangle"""
 
 
 class Rectangle(BaseGeometry):
