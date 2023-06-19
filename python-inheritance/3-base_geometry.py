d785b10c01f019342a1b89654f598873888b0cbb
ba-00001 <ba.00001@outlook.com>
2023-06-19 01:14:04 -0400

    update

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+Module: base_geometry
+
+This module provides an empty class called BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometry-related operations.
+
+    This class does not have any attributes or methods defined.
+    It serves as a base class for other geometry classes.
+    """
+    pass
