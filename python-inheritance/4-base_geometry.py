37a0ed46133d25916d89dd332065cb6c3750d161
ba-00001 <ba.00001@outlook.com>
2023-06-19 01:17:33 -0400

    update

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+Module: base_geometry
+
+This module provides a class called BaseGeometry that serves
+as a base class for geometry-related operations.
+"""
+
+class BaseGeometry:
+    """
+    A base class for geometry-related operations.
+
+    This class provides a placeholder method for calculating
+    the area of a shape. It raises an exception with
+    the message "area() is not implemented" when called.
+    """
+
+    def area(self):
+        """
+        Placeholder method for calculating the area of a shape.
+
+        Raises:
+            Exception: Always raises an exception with
+            the message "area() is not implemented".
+        """
+        raise Exception("area() is not implemented")
