b7e43bd537d1791e0b028e9aab2ca54a12255c86
ba-00001 <ba.00001@outlook.com>
2023-06-19 01:33:49 -0400

    update

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -2,7 +2,8 @@
 """
 Module: base_geometry
 
-This module provides a class called BaseGeometry that serves as a base class for geometry-related operations.
+This module provides a class called BaseGeometry
+ that serves as a base class for geometry-related operations.
 
 Classes:
     - BaseGeometry: A base class for geometry-related operations.
@@ -12,11 +13,13 @@ Exceptions:
     - ValueError: Raised when the value is less than or equal to 0.
 """
 
+
 class BaseGeometry:
     """
     A base class for geometry-related operations.
 
-    This class provides a placeholder method for calculating the area of a shape and a method for validating integer values.
+    This class provides a placeholder method for
+    calculating the area of a shape and a method for validating integer values.
 
     Public Methods:
         - area(self): Placeholder method for calculating the area of a shape.
