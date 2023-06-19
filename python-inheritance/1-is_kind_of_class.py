49d32de3bf38923ab628b12f3444f3dfcd24317f
ba-00001 <ba.00001@outlook.com>
2023-06-19 01:07:55 -0400

    update

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -19,6 +19,7 @@ def is_kind_of_class(obj, a_class):
 
     Returns:
         True if the object is an instance of the specified
-        class or if it is an instance of a class that inherited from the specified class. False otherwise.
+        class or if it is an instance of a class that
+        inherited from the specified class. False otherwise.
     """
     return isinstance(obj, a_class)
