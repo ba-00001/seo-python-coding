639ce177c00ca87317ceef5d66af9ea1f1dc32a7
ba-00001 <124537281+ba-00001@users.noreply.github.com>
2023-06-24 12:50:07 -0400

    update

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-
+"""Module documented"""
 
 
 class BaseGeometry:
-
+    """Module documented"""
 
     def area(self):
-
+        """Module documented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-
+        """Module documented"""
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
-
+    """Module documented"""
 
     def __init__(self, width, height):
-
+        """Module documented"""
         self.__width = width
         self.__height = height
         self.integer_validator("{}".format(width), self.__width)
