13311b45f53f002538cf6b8fa9bc785b4a9d87c6
ba-00001 <124537281+ba-00001@users.noreply.github.com>
2023-06-26 19:42:36 -0400

    update

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -6,39 +6,14 @@ Rectangle = __import__('7-rectangle.py').Rectangle
 
 
 class Square(Rectangle):
-
-    """
-    Square class inherits  the Rectangle
-     class and represents a square shape.
-
-    Methods:
-        N/A
-
-    Attributes:
-        - __size (int): The size of the square.
-    """
+    """ Module"""
 
     def __init__(self, size):
-        """
-        Initializes a Square object with the given size.
-
-        Args:
-            - size (int): The size of the square.
-
-        Raises:
-            - TypeError: If size is not an integer.
-            - ValueError: If size is less than or equal to 0.
-        """
+        """ Module"""
         self.__size = size
         self.integer_validator("size", self.__size)
         super().__init__(self.__size, self.__size)
 
     def __str__(self):
-        """
-        Returns a string representation of the square.
-
-        Returns:
-            - A string describing the square
-            in the format: [Square] <size>/<size>
-        """
+        """ Module"""
         return "[Square] {}/{}".format(self.__size, self.__size)
