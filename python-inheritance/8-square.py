356590517863f526b03e625c9e7b46c476feaa68
ba-00001 <124537281+ba-00001@users.noreply.github.com>
2023-06-26 19:45:15 -0400

    update

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 
+"""Module"""
 
 
 Rectangle = __import__('7-rectangle.py').Rectangle
 
 
 class Square(Rectangle):
-    """ Module"""
+    """Module"""
 
     def __init__(self, size):
-        """ Module"""
+        """Module"""
         self.__size = size
         self.integer_validator("size", self.__size)
         super().__init__(self.__size, self.__size)
 
     def __str__(self):
-        """ Module"""
+        """Module"""
         return "[Square] {}/{}".format(self.__size, self.__size)
