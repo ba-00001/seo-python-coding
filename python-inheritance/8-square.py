6969a4afa9b25cde10763a47b7a832e67a995ca6
ba-00001 <124537281+ba-00001@users.noreply.github.com>
2023-06-26 19:20:50 -0400

    update

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -15,6 +15,7 @@ Rectangle = __import__('7-rectangle.py').Rectangle
 
 
 class Square(Rectangle):
+
     """
     Square class inherits fro_m the Rectangle
      class and represents a square shape.
